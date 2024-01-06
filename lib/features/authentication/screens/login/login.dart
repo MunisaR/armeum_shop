c4e2a29518fee64aaf0bb589edead30c94c4a6b9
MunisaR <munisrahmatullayeva04@gmail.com>
2024-01-06 22:17:14 +0500

    feat: extracted to widgets(clean code)

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -12,7 +12,6 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
